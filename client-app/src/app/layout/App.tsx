2ecac92e9465df449d0edffa1181f22caa4dd3be
Oleg <o.kuzicevs@gmail.com>
2022-10-06 21:47:36 +0300

    add some styling Components

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,30 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import {Header, List} from 'semantic-ui-react';
+import {Container} from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../features/activities/dashboard/ActivityDashboard';
 
 function App() {
 
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/activities").then(response => {
+    axios.get<Activity[]>("http://localhost:5000/api/activities").then(response => {
       setActivities(response.data);
     })
   }, []);
 
   return (
-    <div>
-      <Header as={"h2"} icon="users" content="Reactivities"/>
-        <List>
-          {activities.map((activity: any) => (
-
-            <List.Item 
-              key={activity.id}>{activity.title}
-            </List.Item>
-
-          ))}
-        </List>
-    </div>
+    <Fragment>
+      <NavBar />
+      <Container style={{marginTop: '7em'}}>
+        <ActivityDashboard activities={activities}/>
+      </Container>
+    </Fragment>
   );
 }
 
